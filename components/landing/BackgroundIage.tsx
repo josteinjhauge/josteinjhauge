953e20ccd8fe5436f95827666da487fe72692d6d
Jostein Hauge <josteinjhauge@me.com>
2024-05-01 10:58:06 +0200

    forgot to save

--- components/landing/BackgroundIage.tsx
+++ components/landing/BackgroundIage.tsx
@@ -2,23 +2,6 @@ import React from 'react';
 import Image from 'next/image';
 import { styled } from '@mui/material';
 
-const useStyles = makeStyles({
-    root: {
-        position: 'relative',
-        width: '100%',
-        height: '100vh',
-        overflow: 'hidden',
-    },
-    image: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        objectFit: 'cover',
-    },
-});
-
 const BackgroundImageContainer = styled('div')(({ theme }) => ({
     width: '100%',
     height: '100vh',
